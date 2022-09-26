8c618dd403e34b1e7ac655d45c62cd44a5bd4c75
Julio Guerrero Valle <113936776+JGueVal@users.noreply.github.com>
2022-09-26 20:04:33 +0200

    Update persistence.py

--- persistence.py
+++ persistence.py
@@ -1,53 +1,57 @@
-import math as m
-import itertools as i
+from math import prod
 
-pwrs2 = []
-pwrs3 = []
+PERS = 12
+ran = range(99999, -1, -1)
+
+pwrs9 = []
+pwrs8 = []
 pwrs7 = []
+pwrs6 = [1, 6]
+pwrs3 = [1, 3]
+pwrs2 = [1, 2, 4]
 
-r = range(0,100000)
-
-for c in r:
+for c in range(100000):
+    pw = 9**c
+    pwrs9.append(pw)
+    pw = 8**c
+    pwrs8.append(pw)
     pw = 7**c
     pwrs7.append(pw)
-    pw = 3**c
-    pwrs3.append(pw)
-    pw = 2**c
-    pwrs2.append(pw)
-    print(c,"completo")
+    print(f'{c} completo')
 
-n = 12
+def docprint():
+    with open('perst.txt', 'a') as t:
+        t.write(f'{p9} 9s, {p8} 8s, {p7} 7s, {p6} 6s, {p3} 3s, {p2} 2s\n')
 
-rr = range(99999,-1,-1)
+#   Se puede usar itertools, pero es conveniente esto en caso de querer interrumpir el programa.
+#   EJEMPLO: "if p9 > 99997: continue", "if p9 >= 99997 and p8 > 65535: continue".
 
-for p in i.product(rr, repeat=3):
-    droot = pwrs7[p[0]]*pwrs3[p[1]]*pwrs2[p[2]]
-    count = 1
-    print(p)
-    while count < n:
-        string = [int(dig) for dig in str(droot)]
-        if len(string) == 1:
-            break
-        elif string.count(0) > 0:
-            if count == n-1:
-                t = open("perst.csv","a")
-                t.write("!"+str(p)+" ("+str(m.fsum(p))+")\n")
-                t.close()
-                break
-            else:
-                break
-        elif string.count(5) > 0:
-            if count >= n-2:
-                t = open("perst.csv","a")
-                t.write("!!"+str(p)+" ("+str(m.fsum(p))+")\n")
-                t.close()
-                break
-            else:
-                break
-        else:
-            droot = m.prod(string)
-            count += 1
-    if count == n:
-        t = open("perst.csv","a")
-        t.write(str(p)+" ("+str(m.fsum(p))+")\n")
-        t.close()
+for p9 in ran:
+    for p8 in ran:
+        for p7 in ran:
+            for p6 in range(2):
+                for p3 in range(2):
+                    for p2 in range(3):
+                        if p9+p8+p7+p6+p3+p2 < 20000:
+                            continue
+                        droot = pwrs9[p9] * pwrs8[p8] * pwrs7[p7] * pwrs6[p6] * pwrs3[p3] * pwrs2[p2]
+                        count = 1
+                        print(f'{p9}, {p8}, {p7}, {p6}, {p3}, {p2}')
+                        while count < PERS:
+                            string = [int(dig) for dig in str(droot)]
+                            if len(string) == 1:
+                                break
+                            elif string.count(0) > 0:
+                                if count == PERS-1:
+                                    docprint()
+                                    break
+                                break
+                            elif string.count(5) > 0:
+                                if count >= PERS-2:
+                                    docprint()
+                                    break
+                                break
+                            droot = prod(string)
+                            count += 1
+                        if count == PERS:
+                            docprint()
