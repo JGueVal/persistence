8e8fa96ed63fe2f814c772c9d8e7259d846d13f8
Julio Guerrero Valle <113936776+JGueVal@users.noreply.github.com>
2022-10-11 12:03:42 +0200

    List pwrs6 deleted due to redundancy
    6 = 2*3
    2*3*6 = 2*3*2*3 = 4*9

--- persistence.py
+++ persistence.py
@@ -1,12 +1,15 @@
 from math import prod
 
+###--NECESARIO EN PYTHON 3.10.7--
+#from sys import set_int_max_str_digits
+#set_int_max_str_digits(0)
+
 PERS = 12
 ran = range(99999, -1, -1)
 
 pwrs9 = []
 pwrs8 = []
 pwrs7 = []
-pwrs6 = [1, 6]
 pwrs3 = [1, 3]
 pwrs2 = [1, 2, 4]
 
@@ -20,38 +23,43 @@ for c in range(100000):
     print(f'{c} completo')
 
 def docprint():
-    with open('perst.txt', 'a') as t:
-        t.write(f'{p9} 9s, {p8} 8s, {p7} 7s, {p6} 6s, {p3} 3s, {p2} 2s\n')
+    try:
+        with open('perst.txt', 'a') as t:
+            t.write(f'{p9} 9s, {p8} 8s, {p7} 7s, {p3} 3s, {p2} 2s\n')
+    except:
+        with open('persterr.txt', 'a') as e:
+            e.write(f'ERROR at: {p9} {p8} {p7}\n')
 
 #   Se puede usar itertools, pero es conveniente esto en caso de querer interrumpir el programa.
 #   EJEMPLO: "if p9 > 99997: continue", "if p9 >= 99997 and p8 > 65535: continue".
-
+#   en números más pequeños, buscar si hay un 0 o un 5 lo ralentiza
 for p9 in ran:
     for p8 in ran:
-        for p7 in ran:
-            for p6 in range(2):
-                for p3 in range(2):
-                    for p2 in range(3):
-                        if p9+p8+p7+p6+p3+p2 < 20000:
-                            continue
-                        droot = pwrs9[p9] * pwrs8[p8] * pwrs7[p7] * pwrs6[p6] * pwrs3[p3] * pwrs2[p2]
-                        count = 1
-                        print(f'{p9}, {p8}, {p7}, {p6}, {p3}, {p2}')
-                        while count < PERS:
-                            string = [int(dig) for dig in str(droot)]
-                            if len(string) == 1:
+        p98 = pwrs9[p9] * pwrs8[p8]
+        for p7 in r:
+            p987 = p98 * pwrs7[p7]
+            for p3 in range(2):
+                for p2 in range(3):
+                    if p9+p8+p7+p3+p2 < 20000:
+                        continue
+                    droot = p987 * pwrs3[p3] * pwrs2[p2]
+                    count = 1
+                    print(f'{p9}, {p8}, {p7}, {p3}, {p2}')
+                    while count < PERST:
+                        string = [int(dig) for dig in str(droot)]
+                        if len(string) == 1:
+                            break
+                        elif 0 in string:
+                            if count == PERST-1:
+                                docprint()
                                 break
-                            elif string.count(0) > 0:
-                                if count == PERS-1:
-                                    docprint()
-                                    break
+                            break
+                        elif 5 in string:
+                            if count >= PERST-2:
+                                docprint()
                                 break
-                            elif string.count(5) > 0:
-                                if count >= PERS-2:
-                                    docprint()
-                                    break
-                                break
-                            droot = prod(string)
-                            count += 1
-                        if count == PERS:
-                            docprint()
+                            break
+                        droot = prod(string)
+                        count += 1
+                    if count == PERST:
+                        docprint()
