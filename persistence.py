2a24b5f06209f4f71398536c89112b8109cbbc9d
Julio Guerrero Valle <113936776+JGueVal@users.noreply.github.com>
2022-10-23 21:48:35 +0200

    Optimization
    · 'break' keywords corrected
    · Does not check for fives anymore
    · Printing updates in the same line with flush if it's not a valid number
    · Exception handling
    · List comprehension to get lists of powers, making previous loop unnecessary
    · First range overwritten by range used in nested 'for' loop

--- persistence.py
+++ persistence.py
@@ -1,26 +1,19 @@
 from math import prod
 
-###--NECESARIO EN PYTHON 3.10.7--
+###--NECESARIO A PARTIR DE PYTHON 3.10.7--
 #from sys import set_int_max_str_digits
 #set_int_max_str_digits(0)
 
 PERST = 12
-r = range(99999, -1, -1)
+r = range(100000)
 
-pwrs9 = []
-pwrs8 = []
-pwrs7 = []
+pwrs9 = [9**p9 for p9 in r]
+pwrs8 = [8**p8 for p8 in r]
+pwrs7 = [7**p7 for p7 in r]
 pwrs3 = [1, 3]
 pwrs2 = [1, 2, 4]
 
-for n in range(100000):
-    pw = 9**n
-    pwrs9.append(pw)
-    pw = 8**n
-    pwrs8.append(pw)
-    pw = 7**n
-    pwrs7.append(pw)
-#    print(f'{n} completo')
+r = range(99999, -1, -1)
 
 def docprint():
     try:
@@ -28,11 +21,16 @@ def docprint():
         with open('perst.txt', 'a') as t:
             t.write(f'{p9} 9s, {p8} 8s, {p7} 7s, {p3} 3s, {p2} 2s\n')
     except:
-        input(f'Error at {p9}, {p8}, {p7}, {p3}, {p2}. Press ENTER...')
+        try:
+            print(f'{p9}, {p8}, {p7}, {p3}, {p2}')
+            with open('perstB.txt', 'a') as t:
+                t.write(f'{p9} 9s, {p8} 8s, {p7} 7s, {p3} 3s, {p2} 2s\n')
+        except:
+            print(f'ERROR at: {p9}, {p8}, {p7}, {p3}, {p2}')
 
-#   Se puede usar itertools, pero es conveniente esto en caso de querer interrumpir el programa
-#   EJEMPLO: "if p9 > 99997: continue", "if p9 >= 99997 and p8 > 65535: continue"
-#   en números más pequeños, buscar si hay un 0 o un 5 lo ralentiza
+# Se puede usar itertools, pero es conveniente esto en caso de querer interrumpir el programa
+# EJEMPLO: "if p9 > 99997: continue", "if p9 >= 99997 and p8 > 65535: continue"
+# en números más pequeños, buscar si hay un 0 o un 5 lo ralentiza
 for p9 in r:
     for p8 in r:
         p98 = pwrs9[p9] * pwrs8[p8]
@@ -42,24 +40,19 @@ for p9 in r:
                 for p2 in range(3):
                     if p9+p8+p7+p3+p2 < 20000:
                         continue
-                    droot = p987 * pwrs3[p3] * pwrs2[p2]
+                    step = p987 * pwrs3[p3] * pwrs2[p2]
                     count = 1
-#                    print(f'{p9}, {p8}, {p7}, {p3}, {p2}')
+                    print(f'{p9}, {p8}, {p7}, {p3}, {p2}',
+                        end='\r', flush=True)
                     while count < PERST:
-                        string = [int(dig) for dig in str(droot)]
-                        if len(string) == 1:
+                        numstr = [int(dig) for dig in str(step)]
+                        if len(numstr) == 1:
                             break
-                        elif 0 in string:
+                        elif 0 in numstr:
                             if count == PERST-1:
                                 docprint()
-                                break
                             break
-                        elif 5 in string:
-                            if count >= PERST-2:
-                                docprint()
-                                break
-                            break
-                        droot = prod(string)
+                        step = prod(numstr)
                         count += 1
                     if count == PERST:
                         docprint()
