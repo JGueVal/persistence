7d18af37398a6f98718c79025a3c760e3b805318
Julio Guerrero Valle <113936776+JGueVal@users.noreply.github.com>
2022-10-26 13:56:26 +0200

    Checks numbers containing one or more 5s
    Added checking for 2 persistences under the given persistence.

--- perstwith5.py
+++ perstwith5.py
@@ -1,7 +1,7 @@
 from math import prod
 
 MAXR = int(input("Enter max range.\n \
-    400 has been tried with persistence 6 without success: "))
+    \r400 has been tried with persistence 6 without success: "))
 r = range(MAXR)
 
 pw9 = [9**p9 for p9 in r]
@@ -16,37 +16,40 @@ def docprint(z, y, x, w, v, u):
         with open('perstwith5.txt', 'a') as t:
             t.write(f'{z} 9s, {y} 8s, {x} 7s, {w} 5s, {v} 3s, {u} 2s\n')
     except:
-        with open('errlog.txt', 'a') as e:
-            e.write(f'ERROR at: {z} {y} {x} {w}\n')
+        try:
+            with open('perstwith5.txt', 'a') as t:
+                t.write(f'{z} 9s, {y} 8s, {x} 7s, {w} 5s, {v} 3s, {u} 2s\n')
+        except:
+            print(f'ERROR at: {z} {y} {x} {w} {v} {u}')
 
 def jove(p):
-    for p9 in r:
-        for p8 in r:
-            p98 = pw9[p9] * pw8[p8]
-            for p7 in r:
-                p987 = p98 * pw7[p7]
-                for p5 in range(1,MAXR):
+    for p5 in range(1,MAXR):
+        for p9 in r:
+            p59 = pw5[p5] * pw9[p9]
+            for p8 in r:
+                p598 = p59 * pw8[p8]
+                for p7 in r:
                     for p3 in range(2):
                         for p2 in range(3):
                             print(f'{p9}, {p8}, {p7}, {p5}, {p3}, {p2}', end='\r', flush=True)
-                            dr = p987 * pw5[p5] * pw3[p3] * pw2[p2]
+                            dr = p598 * pw7[p7] * pw3[p3] * pw2[p2]
                             cn = 1
                             while cn < p:
                                 st = [int(d) for d in str(dr)]
                                 if len(st) == 1:
                                     break
                                 elif 0 in st:
-                                    if cn == p-1:
-                                        print(f'{p}: {p9}, {p8}, {p7}, {p5}, {p3}, {p2}')
-                                        #docprint(p9, p8, p7, p5, p3, p2)
-                                        return
+                                    if cn in [p-3, p-2, p-1]:
+                                        print(f'{p-cn}: {p9}, {p8}, {p7}, {p5}, {p3}, {p2}')
+                                        docprint(p9, p8, p7, p5, p3, p2)
+                                        #return
                                     break
                                 dr = prod(st)
                                 cn += 1
-                            if cn == p:
-                                print(f'{p}: {p9}, {p8}, {p7}, {p5}, {p3}, {p2}')
-                                #docprint(p9, p8, p7, p5, p3, p2)
-                                return
+                            if cn in [p-2, p-1, p]:
+                                print(f'{p-cn}: {p9}, {p8}, {p7}, {p5}, {p3}, {p2}')
+                                docprint(p9, p8, p7, p5, p3, p2)
+                                #return
 
 
 # ==============================
