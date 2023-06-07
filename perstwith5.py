82345f36737595497d5db0b3779dd1f0e22b6b57
Julio Guerrero Valle <113936776+JGueVal@users.noreply.github.com>
2023-06-07 14:36:42 +0200

    Potencias de pares quitadas
    Todo número par multiplicado por 5 dará un número terminado en 0. Los sets de números que se multiplican p veces han de ser {2,3,7} ó {3,5,7}.

--- perstwith5.py
+++ perstwith5.py
@@ -5,54 +5,52 @@ MAXR = int(input("Enter max range.\n \
 r = range(MAXR)
 
 pw9 = [9**p9 for p9 in r]
-pw8 = [8**p8 for p8 in r]
 pw7 = [7**p7 for p7 in r]
-pw5 = [5**p5 for p5 in r]
+pw5 = [5**p5 for p5 in r] # inutil multiplicar pares con 5
 pw3 = [1, 3]
-pw2 = [1, 2, 4]
 
-def docprint(z, y, x, w, v, u):
+def docprint(z, x, w, v):
     try:
         with open('perstwith5.txt', 'a') as t:
-            t.write(f'{z} 9s, {y} 8s, {x} 7s, {w} 5s, {v} 3s, {u} 2s\n')
+            t.write(f'{z} 9s, {x} 7s, {w} 5s, {v} 3s\n')
     except:
         try:
             with open('perstwith5.txt', 'a') as t:
-                t.write(f'{z} 9s, {y} 8s, {x} 7s, {w} 5s, {v} 3s, {u} 2s\n')
+                t.write(f'{z} 9s, {x} 7s, {w} 5s, {v} 3s\n')
         except:
-            print(f'ERROR at: {z} {y} {x} {w} {v} {u}')
+            print(f'ERROR at: {z} {x} {w} {v}')
 
-def jove(p):
+def fnProc(p):
     for p5 in range(1,MAXR):
         for p9 in r:
             p59 = pw5[p5] * pw9[p9]
-            for p8 in r:
-                p598 = p59 * pw8[p8]
-                for p7 in r:
-                    for p3 in range(2):
-                        for p2 in range(3):
-                            print(f'{p9}, {p8}, {p7}, {p5}, {p3}, {p2}', end='\r', flush=True)
-                            dr = p598 * pw7[p7] * pw3[p3] * pw2[p2]
-                            cn = 1
-                            while cn < p:
-                                st = [int(d) for d in str(dr)]
-                                if len(st) == 1:
-                                    break
-                                elif 0 in st:
-                                    if cn in [p-3, p-2, p-1]:
-                                        print(f'{p-cn}: {p9}, {p8}, {p7}, {p5}, {p3}, {p2}')
-                                        docprint(p9, p8, p7, p5, p3, p2)
-                                        #return
-                                    break
-                                dr = prod(st)
-                                cn += 1
-                            if cn in [p-2, p-1, p]:
-                                print(f'{p-cn}: {p9}, {p8}, {p7}, {p5}, {p3}, {p2}')
-                                docprint(p9, p8, p7, p5, p3, p2)
-                                #return
+            for p7 in r:
+                for p3 in range(2):
+                    print(f'{p9},_{p7},_{p5},_{p3}_____', end='\r', flush=True)
+                    dr = p59 * pw7[p7] * pw3[p3]
+                    cn = 1
+                    while cn < p:
+                        st = [int(d) for d in str(dr)]
+                        if len(st) == 1:
+                            break
+                        elif 0 in st:
+                            if cn in [p-5, p-4, p-3, p-2, p-1]:
+                                print(f'{cn+1}: {p9}, {p7}, {p5}, {p3}')
+                                docprint(p9, p7, p5, p3)
+#                                return
+                            break
+                        dr = prod(st)
+                        cn += 1
+                    if cn in [p-4, p-3, p-2, p-1, p]:
+                        print(f'{cn}: {p9}, {p7}, {p5}, {p3}')
+                        docprint(p9, p7, p5, p3)
+#                        return
 
 
 # ==============================
 b = int(input("Enter persistence (5 is highest sure hit): "))
-print("P: n, o, s, c, t, d")
-jove(b)
+print(f'Checking persistences from {b} to {b+4}...')
+print("P: n, s, c, t")
+fnProc(b+4)
+
+# IVLIVS mppr.
